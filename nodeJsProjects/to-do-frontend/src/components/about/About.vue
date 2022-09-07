f99d0dfd710840878aa87c98c9605d032a80c679
gyhcom <gyeonho@gamil.com>
2022-09-07 23:14:35 +0900

    경로수정 및 vue Springboot 연결

---
+++ nodeJsProjects/to-do-frontend/src/components/about/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data: () => {
+    return {
+      msg: 'Gyh To Do Application'
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2{
+  font-weight: normal;
+}
+ul{
+  list-style-type: none;
+  padding: 0;
+}
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a{
+  color: #35495E;
+}
+</style>
--- src/main/java/com/todo/ToDoItem/ToDoItemRequest.java
+++ src/main/java/com/todo/ToDoItem/ToDoItemRequest.java
@@ -6,6 +6,8 @@ import lombok.*;
 
 @Getter @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToDoItemRequest {
 
     private String id;
